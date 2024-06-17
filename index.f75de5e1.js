43d1f7b6031a8132bbc52d5f14a24b412558a3cc
verjane23 <verjane23.gmail.com>
2024-06-17 19:39:55 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const table = document.querySelector("table");
+const rowElements = table.querySelectorAll("tr");
+rowElements.forEach((elem)=>{
+    const copyElem = elem.cells[1].cloneNode(true);
+    elem.cells[3].after(copyElem);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
